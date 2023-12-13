11c1b9edb413f82dc7e675b1f862f847a6138a4d
adarshhlohar <adarshhlohar@gmail.com>
2023-12-13 17:25:46 +0530

    I am Created a add Hotel in a Hotel Reservation System with Name and rates for Regular Customer..

--- src/main/java/com/bridgelabz/HostelReservationSystem.java
+++ src/main/java/com/bridgelabz/HostelReservationSystem.java
@@ -1,7 +1,25 @@
 package com.bridgelabz;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class HostelReservationSystem {
 	public static void main(String[] args) {
 		System.out.println("Welcom to Hotel Reservation System !!");
+		ArrayList<Hotels> hotelList = new ArrayList<>();
+		Hotels[] addHotels = {
+				new Hotels("Lakewood", 3),
+				new Hotels("Bridgewood", 4),
+				new Hotels("Ridgewood", 5)
+		};
+		
+		for(Hotels hotel : addHotels) {
+			hotelList.add(hotel);
+		}
+		
+		Iterator<Hotels> itr = hotelList.iterator();
+		while(itr.hasNext()) {
+			System.out.println(itr.next());
+		}
 	}
 }
